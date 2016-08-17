f2a6319430e659cd668af34365bf95c81b88cba9
Danette Klein <danetteklein@gmail.com>
2016-08-16 20:01:21 -0700

    Committing files for Assignmnent11

---
+++ part3/fewdCourses.js
@@ -0,0 +1,29 @@
+{
+	"teammembers": [
+		{
+			"class": "COMM641",
+			"instructor": "Kay/Ruvalcaba/Richard",
+			"description": "class 1"
+	},
+		{
+			"class": "COMM642",
+			"instructor": "Kay",
+			"description": "class 2"
+	},
+		{
+			"class": "COMM643",
+			"instructor": "Ruvalcaba",
+			"description": "class 3"
+	},
+		{
+			"class": "COMM644",
+			"instructor": "Kay/Ruvalcaba",
+			"description": "class 4"
+	},
+		{
+			"class": "COMM645",
+			"instructor": "Kay",
+			"description": "class 5"
+	}
+]
+}
